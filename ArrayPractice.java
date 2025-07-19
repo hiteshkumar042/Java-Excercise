7bc17ccbc3ed424ac0d6d052af00d58a71cb8333
hiteshkumar042 <hksolanki78@gmail.com>
2025-07-19 19:08:52 +0530

    array min max

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArrayPractice {
 
     // Find Larget Number in Array
@@ -22,11 +24,32 @@ public class ArrayPractice {
         return min;
     }
 
+    // Find the Smallest and largest element in an array
+    static int[] maxMinNumber(int[] arr) {
+        if (arr == null || arr.length == 0) {
+            throw new IllegalArgumentException("Array should not be blank");
+        }
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        return new int[] { min, max };
+    }
+
     public static void main(String[] args) {
         // Find Larget Number in Array
         int[] arr = { 10, 15, 80, 25, 60 };
         System.out.println(largetNumber(arr));
         // Find Smallest Number in Array
         System.out.println(smalletNumber(arr));
+        // Find the Smallest and largest element in an array
+        System.out.println(Arrays.toString((maxMinNumber(arr))));
+
     }
 }
