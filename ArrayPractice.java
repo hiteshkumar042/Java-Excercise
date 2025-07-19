8853729b0d2bc9efb3d635bbceee75f5c3a322b0
hiteshkumar042 <hksolanki78@gmail.com>
2025-07-19 17:12:29 +0530

    min number in an array

--- ArrayPractice.java
+++ ArrayPractice.java
@@ -1,5 +1,6 @@
 public class ArrayPractice {
 
+    // Find Larget Number in Array
     static int largetNumber(int[] arr) {
         int max = arr[0];
         for (int i = 0; i < arr.length; i++) {
@@ -10,9 +11,22 @@ public class ArrayPractice {
         return max;
     }
 
+    // Find smallest Number in Array
+    static int smalletNumber(int[] arr) {
+        int min = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
     public static void main(String[] args) {
         // Find Larget Number in Array
         int[] arr = { 10, 15, 80, 25, 60 };
         System.out.println(largetNumber(arr));
+        // Find Smallest Number in Array
+        System.out.println(smalletNumber(arr));
     }
 }
